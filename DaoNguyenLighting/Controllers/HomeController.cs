7519f87fab733b84af3d7719f8775e525c7993e4
Pham Duc Thanh <phm.duc.thanh@gmail.com>
2018-04-12 14:29:28 +0700

    first commit

---
+++ DaoNguyenLighting/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DaoNguyenLighting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Product()
+        {
+            ViewBag.Message = "Product Page";
+
+            return View();
+        }
+
+        public ActionResult Trending()
+        {
+            ViewBag.Message = "Home Page";
+
+            return View();
+        }
+        public ActionResult New()
+        {
+            ViewBag.Message = "News Page";
+
+            return View();
+        }
+    }
+}
